97ceef69ac58955044bf8435b211d63370093fd9
Gabriel S Cruz <gabrielscruz.g81@gmail.com>
2026-08-03 22:10:23 -0300

    :hammer: change redirect to local cora auth.

--- frontend/src/app/[locale]/callback/cora/route.ts
+++ frontend/src/app/[locale]/callback/cora/route.ts
@@ -41,14 +41,14 @@ export async function GET(
       text: `Locale: ${locale}\nCode: ${code}\nState: ${state}`,
     })
 
-    // REDIRECT MÁGICO PARA A APLICAÇÃO QUE ESTÁ RODANDO O PROJETO
-    //if (state) {
-    //  return NextResponse.redirect(`http://localhost:8090/pt-BR/callback/cora?code=${code}&state=${state}`);
-    //}
-
+    // REDIRECT PARA A APLICAÇÃO QUE ESTÁ RODANDO O PROJETO
     if (state) {
-      return NextResponse.redirect(`https://gugga-ss-api.g81.com.br/pt-BR/callback/cora?code=${code}&state=${state}`);
+      return NextResponse.redirect(`http://localhost:8080/pt-BR/callback/cora?code=${code}&state=${state}`);
     }
+
+    /*     if (state) {
+      return NextResponse.redirect(`https://gugga-ss-api.g81.com.br/pt-BR/callback/cora?code=${code}&state=${state}`);
+    } */
 
     return NextResponse.json({ success: true })
   } catch (error: any) {
